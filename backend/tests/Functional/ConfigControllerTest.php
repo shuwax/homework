f907013a461280dcc22b4a4570437fcf445e1acc
sebastiancytawa <sebastian.cytawa@gmail.com>
2022-02-15 14:37:46 +0100

    first Test

---
+++ backend/tests/Functional/ConfigControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use ApiTestCase\JsonApiTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class ConfigControllerTest extends JsonApiTestCase
+{
+
+    public function testAliveResponse() {
+        $this->client->request('GET','/api/config/alive');
+        $response = $this->client->getResponse();
+//
+        $this->assertResponse($response, 'alive', Response::HTTP_OK);
+
+    }
+}
