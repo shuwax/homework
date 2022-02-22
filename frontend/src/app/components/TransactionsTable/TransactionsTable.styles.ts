47fed85118bcd82779dd9cc15692ca4a7da4105b
Sebastian Cytawa <sebastian.cytawa@edg1s.com>
2022-02-22 13:56:02 +0000

    fix test

--- frontend/src/app/components/TransactionsTable/TransactionsTable.styles.ts
+++ frontend/src/app/components/TransactionsTable/TransactionsTable.styles.ts
@@ -1,10 +1,8 @@
-import { makeStyles, Theme } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
 
-export const useStyles = makeStyles(({ maxHeightTable }: Theme) => ({
+export const useStyles = makeStyles(() => ({
   tableContainer: {
-    margin: "0 20px",
-    width: "calc(100% - 40px)",
-    maxHeight: maxHeightTable,
+    width: "100%",
   },
   buttonsTableCell: {
     padding: 5,
